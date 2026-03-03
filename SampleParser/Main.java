25c301573cb4c9fa2e019ea7336c088e45e26498
Ashmit Mittal <amitta01@uoguelph.ca>
2026-03-02 19:51:46 -0500

    Added Error Handeling

--- SampleParser/Main.java
+++ SampleParser/Main.java
@@ -1,5 +1,5 @@
 /*
-  Created by: Fei Song
+  Created by: Fei Song, Silas Wright
   File Name: Main.java
   To Build: 
   After the Scanner.java, tiny.flex, and tiny.cup have been processed, do:
@@ -16,19 +16,62 @@ import absyn.*;
    
 class Main {
   public final static boolean SHOW_TREE = true;
-  static public void main(String argv[]) {    
-    /* Start the parser */
-    try {
-      parser p = new parser(new Lexer(new FileReader(argv[0])));
-      Absyn result = (Absyn)(p.parse().value);      
-      if (SHOW_TREE && result != null) {
-         System.out.println("The abstract syntax tree is:");
-         AbsynVisitor visitor = new ShowTreeVisitor();
-         result.accept(visitor, 0); 
+
+  public static String toAbsFileName(String inputFileName) {
+      int dotIndex = inputFileName.lastIndexOf('.');
+      
+      String baseName;
+      if (dotIndex > 0) {
+          baseName = inputFileName.substring(0, dotIndex);
+      } else {
+          baseName = inputFileName;  // no extension found
       }
-    } catch (Exception e) {
-      /* do cleanup here -- possibly rethrow e */
-      e.printStackTrace();
+
+      System.out.println(baseName + ".abs");
+
+      return baseName + ".abs";
+  }
+  static public void main(String argv[]) {   
+
+    if (argv.length < 2) {
+      System.err.println("Usage: java Main <-a|-s|-c> <inputfile>");
+      return;
+    }
+
+    String flag = argv[0];  // take flag
+    String inputfile = argv[1]; // store filename
+
+    //dynamically set output file based on inputfile
+    try {
+      PrintStream fileOut = new PrintStream(new File(toAbsFileName(inputfile)));
+      System.setOut(fileOut); // set stdout to output file
+  } catch (FileNotFoundException e) {
+      System.err.println("Error: Cannot create or write to file '" + inputfile + "'");
+  }
+    if (flag.equals("-a")) {
+        /* Start the parser */
+        try {
+          parser p = new parser(new Lexer(new FileReader(inputfile)));
+          Absyn result = (Absyn)(p.parse().value);      
+          if (SHOW_TREE && result != null) {
+            System.out.println("The abstract syntax tree is:");
+            AbsynVisitor visitor = new ShowTreeVisitor();
+            result.accept(visitor, 0); 
+          }
+        } catch (Exception e) {
+          /* do cleanup here -- possibly rethrow e */
+          e.printStackTrace();
+        }
+    }
+    else if (flag.equals("-s")) {
+        //checkpoint 2
+    }
+    else if (flag.equals("-c")) {
+        //checkpoint 3
+    }
+    else {
+        System.err.println("Unknown flag: " + flag);
+        System.err.println("Use -a, -s, or -c");
     }
   }
 }
